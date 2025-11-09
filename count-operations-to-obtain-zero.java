1bf1ba8fb2b7342e63d1e2726c49ae7b79e5c600
Anup Kumar Singh <121098138+geeky2311anup@users.noreply.github.com>
2025-11-09 20:41:35 +0530

    Create count-operations-to-obtain-zero.java

---
+++ count-operations-to-obtain-zero.java
@@ -0,0 +1,15 @@
+//PROBLEM LINK -> https://leetcode.com/problems/count-operations-to-obtain-zero/?envType=daily-question&envId=2025-11-09
+
+class Solution {
+    public int countOperations(int n1, int n2) {
+        int c = 0;
+        while (n1 > 0 && n2 > 0) {
+            c += n1 / n2;
+            n1 %= n2;
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+        return c;
+    }
+}
